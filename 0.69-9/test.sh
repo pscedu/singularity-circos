d128b9687b87ee57e767c2218a4b3d3b68a37ba1
Ivan Cao-Berg <icaoberg@psc.edu>
2021-12-18 15:30:04 -0500

    Updated test

--- 0.69-9/test.sh
+++ 0.69-9/test.sh
@@ -1,5 +1,31 @@
-#!/bin/bash
+]#!/bin/bash
 
 set -x
 
-tree .
+shopt -s expand_aliases
+
+if [ ! -d circos-tutorials-0.67 ]; then
+	wget -nc wget http://circos.ca/distribution/circos-tutorials-current.tgz
+	tar -xvf circos-tutorials-current.tgz && rm -fv circos-tutorials-current.tgz
+fi
+
+alias circos='singularity exec "$DIRECTORY"/singularity-circos-0.69-9.sif circos'
+
+DIRECTORY=$(pwd)
+cd circos-tutorials-0.67/tutorials/2/2
+circos -conf circos.conf
+
+EXIT_CODE=0
+if [ ! -f circos.png ]; then
+	EXIT_CODE=1
+fi
+
+cd ../3
+circos -conf circos.conf
+
+EXIT_CODE=0
+if [ ! -f circos.png ]; then
+       	EXIT_CODE=1
+fi
+
+exit $EXIT_CODE
