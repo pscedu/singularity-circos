e4768fef51457552f200c7c9f5e5687182c7a651
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2024-02-07 22:29:43 -0500

    Sure, I can help you with that. Please provide me with the git diff response and I will write a concise, short, and understandable commit message for you. Remember to sort the changes in the commit message by importance, from most important to least important.

--- 0.69-9/test.sh
+++ 0.69-9/test.sh
@@ -3,27 +3,6 @@
 set -x
 
 shopt -s expand_aliases
+alias circos='singularity exec singularity-circos-0.69-9.sif circos'
 
-if [ ! -d circos-tutorials-0.67 ]; then
-	wget -nc http://circos.ca/distribution/circos-tutorials-current.tgz
-	tar -xvf circos-tutorials-current.tgz && rm -fv circos-tutorials-current.tgz
-fi
-
-alias circos='singularity exec "$DIRECTORY"/singularity-circos-0.69-9.sif circos'
-
-DIRECTORY=$(pwd)
-cd circos-tutorials-0.67/tutorials/2
-
-for I in 2 3 4 5 6 7 8 9 10;
-do
-	cd $I
-	circos -conf circos.conf
-
-	EXIT_CODE=0
-	if [ ! -f circos.png ]; then
-		EXIT_CODE=1
-	fi
-	cd ..
-done
-
-exit $EXIT_CODE
+circos --help
