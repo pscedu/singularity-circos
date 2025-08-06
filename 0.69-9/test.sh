b246423667f838a0dfecc4798a24352c3d1e4073
icaoberg <icaoberg@gmail.com>
2025-08-06 17:13:38 -0400

    Update test.sh

--- 0.69-9/test.sh
+++ 0.69-9/test.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 set -x
-
 shopt -s expand_aliases
 alias circos='singularity exec singularity-circos-0.69-9.sif circos'
 
-circos --help
+# Run circos and check status
+if circos --help; then
+    exit 0
+else
+    exit 1
+fi
